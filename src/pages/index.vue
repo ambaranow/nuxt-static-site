2dba65820a33152380dcaf6c9eaed8efc9b3ced1
ambaranow <a.baranov@notamedia.ru>
2017-09-21 19:04:48 +0300

    add pug and scss support

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,19 +1,20 @@
-<template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
+<template lang="pug">
+  section.container
+    div
+      logo
+      h1.title
         vue-static-site
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+      h2.subtitle
+        | Nuxt.js project
+      .links
+        a.button--green(
+          href="https://nuxtjs.org/"
+          target="_blank")
+          | Documentation
+        a.button--grey(
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank")
+          | GitHub
 </template>
 
 <script>
@@ -26,33 +27,30 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .container {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+  .links {
+    padding-top: 15px;
+  }
 }
 </style>
