c0bdaeeaacc58bdd244142d79edd99e3dc0fbd06
ambaranow <a.baranov@notamedia.ru>
2017-09-21 23:22:44 +0300

    add Webpack OpenBrowserPlugin, fix logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,34 +1,48 @@
+const OpenBrowserPlugin = require('open-browser-webpack-plugin')
+
 module.exports = {
   /*
-  ** root for source code
-  */
+   ** root for source code
+   */
   srcDir: 'src/',
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: 'vue-static-site',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt.js project'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
   /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     extractCSS: true, // doesn`t work
     /*
-    ** Run ESLint on save
-    */
+     ** Run ESLint on save
+     */
     extend (config, ctx) {
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
@@ -37,6 +51,11 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
+        config.plugins.push(
+          new OpenBrowserPlugin({
+            url: 'http://localhost:3000'
+          })
+        )
       }
     }
   }
--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
   .VueToNuxtLogo
-    .Triangle Triangle--two
-    .Triangle Triangle--one
-    .Triangle Triangle--three
-    .Triangle Triangle--four
+    .Triangle.Triangle--two
+    .Triangle.Triangle--one
+    .Triangle.Triangle--three
+    .Triangle.Triangle--four
 </template>
 
 <style lang="scss">
@@ -21,39 +21,39 @@
     left: 0;
     width: 0;
     height: 0;
-  }
+    &--one {
+      border-left: 105px solid transparent;
+      border-right: 105px solid transparent;
+      border-bottom: 180px solid #41B883;
+    }
 
-  .Triangle--one {
-    border-left: 105px solid transparent;
-    border-right: 105px solid transparent;
-    border-bottom: 180px solid #41B883;
-  }
+    &--two {
+      top: 30px;
+      left: 35px;
+      animation: goright 0.5s linear forwards 3.5s;
+      border-left: 87.5px solid transparent;
+      border-right: 87.5px solid transparent;
+      border-bottom: 150px solid #3B8070;
+    }
 
-  .Triangle--two {
-    top: 30px;
-    left: 35px;
-    animation: goright 0.5s linear forwards 3.5s;
-    border-left: 87.5px solid transparent;
-    border-right: 87.5px solid transparent;
-    border-bottom: 150px solid #3B8070;
-  }
+    &--three {
+      top: 60px;
+      left: 35px;
+      animation: goright 0.5s linear forwards 3.5s;
+      border-left: 70px solid transparent;
+      border-right: 70px solid transparent;
+      border-bottom: 120px solid #35495E;
+    }
 
-  .Triangle--three {
-    top: 60px;
-    left: 35px;
-    animation: goright 0.5s linear forwards 3.5s;
-    border-left: 70px solid transparent;
-    border-right: 70px solid transparent;
-    border-bottom: 120px solid #35495E;
-  }
+    &--four {
+      top: 120px;
+      left: 70px;
+      animation: godown 0.5s linear forwards 3s;
+      border-left: 35px solid transparent;
+      border-right: 35px solid transparent;
+      border-bottom: 60px solid #fff;
+    }
 
-  .Triangle--four {
-    top: 120px;
-    left: 70px;
-    animation: godown 0.5s linear forwards 3s;
-    border-left: 35px solid transparent;
-    border-right: 35px solid transparent;
-    border-bottom: 60px solid #fff;
   }
 }
 
